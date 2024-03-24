c57299b8b06207e45e8bc72258b2927f4305ca76
abel mengich <kipyegoabel2308@gmail.com>
2024-03-23 22:46:39 -0400

    test passed

---
+++ src/00-objectball.js
@@ -0,0 +1,164 @@
+function mostPointsScored(players) {
+    let maxPoints = 0;
+    let playerWithMostPoints = '';
+
+    for (let player of players) {
+        if (player.points > maxPoints) {
+            maxPoints = player.points;
+            playerWithMostPoints = player.name;
+        }
+    }
+
+    return playerWithMostPoints;
+}
+
+function winningTeam(teams) {
+    let maxPoints = 0;
+    let teamWithMostPoints = '';
+
+    for (let team of teams) {
+        let totalPoints = 0;
+        for (let player of team.players) {
+            totalPoints += player.points;
+        }
+        if (totalPoints > maxPoints) {
+            maxPoints = totalPoints;
+            teamWithMostPoints = team.name;
+        }
+    }
+
+    return teamWithMostPoints;
+}
+
+function playerWithLongestName(players) {
+    let longestName = '';
+    for (let player of players) {
+        if (player.name.length > longestName.length) {
+            longestName = player.name;
+        }
+    }
+    return longestName;
+}
+function mostPointsScored(players) {
+    let maxPoints = 0;
+    let playerWithMostPoints = '';
+
+    for (let player of players) {
+        if (player.points > maxPoints) {
+            maxPoints = player.points;
+            playerWithMostPoints = player.name;
+        }
+    }
+
+    return playerWithMostPoints;
+}
+
+function winningTeam(teams) {
+    let maxPoints = 0;
+    let teamWithMostPoints = '';
+
+    for (let team of teams) {
+        let totalPoints = 0;
+        for (let player of team.players) {
+            totalPoints += player.points;
+        }
+        if (totalPoints > maxPoints) {
+            maxPoints = totalPoints;
+            teamWithMostPoints = team.name;
+        }
+    }
+
+    return teamWithMostPoints;
+}
+
+function playerWithLongestName(players) {
+    let longestName = '';
+    for (let player of players) {
+        if (player.name.length > longestName.length) {
+            longestName = player.name;
+        }
+    }
+    return longestName;
+}
+function mostPointsScored(players) {
+    let maxPoints = 0;
+    let playerWithMostPoints = '';
+
+    for (let player of players) {
+        if (player.points > maxPoints) {
+            maxPoints = player.points;
+            playerWithMostPoints = player.name;
+        }
+    }
+
+    return playerWithMostPoints;
+}
+
+function winningTeam(teams) {
+    let maxPoints = 0;
+    let teamWithMostPoints = '';
+
+    for (let team of teams) {
+        let totalPoints = 0;
+        for (let player of team.players) {
+            totalPoints += player.points;
+        }
+        if (totalPoints > maxPoints) {
+            maxPoints = totalPoints;
+            teamWithMostPoints = team.name;
+        }
+    }
+
+    return teamWithMostPoints;
+}
+
+function playerWithLongestName(players) {
+    let longestName = '';
+    for (let player of players) {
+        if (player.name.length > longestName.length) {
+            longestName = player.name;
+        }
+    }
+    return longestName;
+}
+function mostPointsScored(players) {
+    let maxPoints = 0;
+    let playerWithMostPoints = '';
+
+    for (let player of players) {
+        if (player.points > maxPoints) {
+            maxPoints = player.points;
+            playerWithMostPoints = player.name;
+        }
+    }
+
+    return playerWithMostPoints;
+}
+
+function winningTeam(teams) {
+    let maxPoints = 0;
+    let teamWithMostPoints = '';
+
+    for (let team of teams) {
+        let totalPoints = 0;
+        for (let player of team.players) {
+            totalPoints += player.points;
+        }
+        if (totalPoints > maxPoints) {
+            maxPoints = totalPoints;
+            teamWithMostPoints = team.name;
+        }
+    }
+
+    return teamWithMostPoints;
+}
+
+function playerWithLongestName(players) {
+    let longestName = '';
+    for (let player of players) {
+        if (player.name.length > longestName.length) {
+            longestName = player.name;
+        }
+    }
+    return longestName;
+}
